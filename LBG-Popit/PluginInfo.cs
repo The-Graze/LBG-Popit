a98c1b28268c132cad2ec0d522c0cf69f12e951b
Graze <reece2004f@gmail.com>
2022-06-30 09:53:07 +0100

    Add project files.

---
+++ LBG-Popit/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace LBG_Popit
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.graze.gorillatag.lbg-popit";
+        public const string Name = "LBG_Popit";
+        public const string Version = "1.0.0";
+    }
+}
